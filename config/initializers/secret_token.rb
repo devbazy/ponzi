a7cdfd4951a1bfe78c1d5bba8815d322f2c89394
jb41 <jb41.mail@gmail.com>
2013-09-28 14:29:40 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ponzi::Application.config.secret_token = '3940055ef8c169e45d02c650c0f2cb4332680f51887b57bef552752de601c29ce5393363fe959c6238b6fa25808ff89e24fecb376b5068e731b44000573ea5ad'
